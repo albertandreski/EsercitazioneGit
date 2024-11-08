8e4e598ef03a5a89057d3dcab2c38c97a9019b1b
net <net@SIS-10.igalvani.local>
2024-11-08 10:52:09 +0100

    aggiunta proggramino

---
+++ programma.py
@@ -0,0 +1,11 @@
+print("inserisci quanti numeri vuoi per interrompere e mettere 0")
+w = 0
+s = 0
+while w==0
+x=int8(input("metti un numero|n"))
+if x!=0:
+    s+=x
+    else:
+        w=1
+        print(if"questa è la tua somma {s}")
+        
